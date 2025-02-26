6c96a6a4e05d8f727b1806a54e82f3a99b543bab
Arasaka <bahaeddin365@gmail.com>
2025-02-26 23:13:13 +0330

    basic structure of mobile navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
+import { FaBars } from 'react-icons/fa6';
+import { FaXmark } from 'react-icons/fa6';
 
 function Navbar() {
   return (
-    <nav id="nav">
-      <div className="nav__logo"></div>
+    <nav id="nav" className="cu-container">
+      <div className="nav__logo">
+        <img src="/images/logo.png" alt="logo" />
+        <h1 className="nav__logo-text">NOVA</h1>
+      </div>
       <ul className="mobile-nav__list">
+        <FaXmark className="mobile-nav__ham-icon" />
         <li className="mobile-nav__list-item">
           <a href="#" className="mobile-nav__link">
             Home
@@ -34,6 +40,7 @@ function Navbar() {
       <a href="#" className="nav__cta-btn">
         Get Started
       </a>
+      <FaBars className="mobile-nav__ham-icon" />
     </nav>
   );
 }
