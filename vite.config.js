95ed510186cf535d5457bc2bfc3754546f4a6c9b
Arasaka <bahaeddin365@gmail.com>
2025-04-25 02:42:47 +0330

    Add homepage and deploy script to package.json; update Vite config for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,13 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
     port: 3000,
+  },
+  base: '/Nova-landingPage/', // برای GitHub Pages
+  preview: {
+    base: '/', // برای تست محلی
   },
 });
